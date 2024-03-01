e552d150f5fdc85cee7bab93323426665571ab7c
Abdur Rafay Ahmed <a_rafay01@hotmail.com>
2024-03-01 13:02:29 +0500

    db connection

--- server/app.js
+++ server/app.js
@@ -1,7 +1,23 @@
 import express from "express"
-
+import dotenv from 'dotenv'
+import mongoose from "mongoose";
 
 const app = express();
+
+
+dotenv.config({path: "./.env"})
+const db = process.env.DB
+
+mongoose.connect(db).then(()=>{
+    console.log(`connection successfull`);
+}).catch((err)=> console.log(`no connection`));
+
+//MIDDLEWARE
+const middleware = (req,res,next) =>{
+    console.log('heelo my middleware');
+    next();
+}
+
 
 app.get('/', (req,res)=>{
     res.send('hello world from server');
@@ -9,7 +25,7 @@ app.get('/', (req,res)=>{
 app.get('/about', (req,res)=>{
     res.send('hello about world from server');
 })
-app.get('/contact', (req,res)=>{
+app.get('/contact',middleware, (req,res)=>{
     res.send('hello contact world from server');
 })
 app.get('/signin', (req,res)=>{
