c85db8f1660f40c3131db6d806566914a12bc9c7
Abdur Rafay Ahmed <a_rafay01@hotmail.com>
2024-03-01 17:10:58 +0500

    created modelSchema

--- server/app.js
+++ server/app.js
@@ -1,39 +1,45 @@
-import express from "express"
-import dotenv from 'dotenv'
-import mongoose from "mongoose";
+import express from "express";
+import dotenv from "dotenv";
+import connectDB from "./db/index.js";
 
 const app = express();
 
-
-dotenv.config({path: "./.env"})
-const db = process.env.DB
-
-mongoose.connect(db).then(()=>{
-    console.log(`connection successfull`);
-}).catch((err)=> console.log(`no connection`));
+dotenv.config({ path: "./.env" });
 
 //MIDDLEWARE
-const middleware = (req,res,next) =>{
-    console.log('heelo my middleware');
-    next();
-}
+const middleware = (req, res, next) => {
+  console.log("heelo my middleware");
+  next();
+};
+
+app.get("/", (req, res) => {
+  res.send("hello world from server");
+});
+app.get("/about", (req, res) => {
+  res.send("hello about world from server");
+});
+app.get("/contact", middleware, (req, res) => {
+  res.send("hello contact world from server");
+});
+app.get("/signin", (req, res) => {
+  res.send("hello signin world from server");
+});
+app.get("/signup", (req, res) => {
+  res.send("hello signup world from server");
+});
 
 
-app.get('/', (req,res)=>{
-    res.send('hello world from server');
+const port = process.env.PORT;
+connectDB()
+.then(()=>{
+    app.listen(port, ()=>{
+        console.log(`📔 Server is running at port : ${port}`);
+        // app.on("error", (error)=>{
+        //     console.log("ERRR:", error);
+        //     throw error;
+        // })
+    })
 })
-app.get('/about', (req,res)=>{
-    res.send('hello about world from server');
-})
-app.get('/contact',middleware, (req,res)=>{
-    res.send('hello contact world from server');
-})
-app.get('/signin', (req,res)=>{
-    res.send('hello signin world from server');
-})
-app.get('/signup', (req,res)=>{
-    res.send('hello signup world from server');
-})
-app.listen(3001, ()=>{
-    console.log('server running at 3001');
+.catch((error)=>{
+    console.log("MONGO db connection failed!!", error);
 })
---
+++ server/models/userSchema.js
@@ -0,0 +1,93 @@
+import mongoose, { Schema } from "mongoose";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+
+const userSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+      trim: true,
+      index: true,
+    },
+
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+      trim: true,
+    },
+
+    phone: {
+      type: Number,
+      required: true,
+    },
+
+    work: {
+      type: String, 
+      required: true,
+    },
+
+    password: {
+      type: String, 
+      required: true
+    },
+
+    cpassword: {
+        type: String, 
+        required: true
+      },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+// userSchema.pre("save", async function (next) {
+//   if (!this.isModified("password")) return next();
+
+//   this.password = await bcrypt.hash(this.password, 10);
+//   // do stuff
+//   next();
+// });
+
+// userSchema.methods.isPasswordCorrect = async function (password) {
+//   return await bcrypt.compare(password, this.password);
+// };
+
+// userSchema.methods.generateAccessToken = function() {
+//     return jwt.sign(
+//         {
+//             __id: this._id,
+//             email: this.email,
+//             username:this.username,
+//             fullname: this.fullname
+//         },
+//         process.env.ACCESS_TOKEN_SECRET,
+
+//         {
+//             expiresIn: process.env.ACCESS_TOKEN_EXPIRY
+//         }
+//     )
+// }
+
+// userSchema.methods.generateRefreshToken = function() {
+//     return jwt.sign(
+//         {
+//             __id: this._id,
+//         },
+//         process.env.REFRESH_TOKEN_SECRET,
+
+//         {
+//             expiresIn: process.env.REFRESH_TOKEN_EXPIRY
+//         }
+//     )
+// }
+
+
+
+
+export const User = mongoose.model("User", userSchema);
